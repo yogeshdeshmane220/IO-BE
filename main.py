f3552d5d491bfb5c82a7a9bc4a883d6020dbd8e9
yogesh7122002 <yogeshdeshmane21@gmail.com>
2025-11-20 01:49:07 +0530

    changing files

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173", "*"],
+    allow_origins=["http://13.60.232.38:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
